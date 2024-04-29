7ae534606c56b93697432a4158b2346e97a19729
ericarocca <156796666+ericarocca@users.noreply.github.com>
2024-04-29 16:44:37 +0200

    Update function.py

--- function.py
+++ function.py
@@ -14,10 +14,17 @@ def heatCN(length, nx, time, nt, alpha):
 
     #function of temperature
     function_temperature = np.sin( np.pi * x_grid / length)
-#    function_temperature = np.cos(np.pi * x_grid)
+    #function_temperature = np.cos(np.pi * x_grid)
     
-    #create heat array
+    #create heat array 
     w = np.zeros([nx, nt])
+    
+    #create matrices for calculation
+    A = np.eye(nx)
+    B = np.eye(nx)
+
+    #create vector b of boundary conditions
+    b = np.zeros(nx)
     
     for i in range(nx):
         w[i,:] = function_temperature[i]
@@ -27,37 +34,36 @@ def heatCN(length, nx, time, nt, alpha):
     
     #calculate coefficient
     deltax = length / (nx - 1)
-    deltat = time / (nt - 1)
-    r = alpha * deltat / (deltax)**2
     
-    #create matrices    
-    A = np.eye(nx) - ((np.eye(nx, k=1) + np.eye(nx, k=-1) - (1 / r + 2) * np.eye(nx)) * r)
-    B = np.eye(nx) + ((np.eye(nx, k=1) + np.eye(nx, k=-1) + (1 / r - 2) * np.eye(nx)) * r)
-    
-    for i in range(nt):
+    if nt > 1:
+        deltat = time / (nt - 1)
+        r = alpha * deltat / (deltax) ** 2
         
-        #vector b of boundary conditions in RHS
-        b = np.zeros(nx)
-        b[0] = r * 2 * w[0,i]
-        b[-1] = r * 2 * w[-1,i]
+        #create matrices
+        A = np.eye(nx) - ((np.eye(nx, k=1) + np.eye(nx, k=-1) - (1 / r + 2) * np.eye(nx)) * r)
+        B = np.eye(nx) + ((np.eye(nx, k=1) + np.eye(nx, k=-1) + (1 / r - 2) * np.eye(nx)) * r)
         
-        #initialize d with the same shape as w[:, i]
-        d = np.zeros_like(w[:, i])  
-        
-        #compute matrix-vector product between B and the i-th column of w
-        for j in range(nx):
-            d[j] = np.dot(B[j], w[:, i])
+        for i in range(nt):
+            #vector b of boundary conditions in RHS
+            b[0] = r * 2 * w[0,i]
+            b[-1] = r * 2 * w[-1,i]
             
-        #add the boundary conditions
-        d[0] += b[0]
-        d[-1] += b[-1]
-
-        
-        #solve
-        w[:,i] = np.linalg.solve(A, d)
-        
-        #impose boundary conditions
-        w[0,:] = w[-1,:] = 0
+            #initialize d with the same shape as w[:, i]
+            d = np.zeros_like(w[:, i])  
+            
+            #compute matrix-vector product between B and the i-th column of w
+            for j in range(nx):
+                d[j] = np.dot(B[j], w[:, i])
+                
+            #add the boundary conditions
+            d[0] += b[0]
+            d[-1] += b[-1]
+            
+            
+            #solve
+            w[:,i] = np.linalg.solve(A, d)
+            #impose boundary conditions
+            w[0,:] = w[-1,:] = 0
         
     
     #return solution    
