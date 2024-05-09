baec7f3dc14f09bf9f7deaa72ea6b0a913b92827
ericarocca <156796666+ericarocca@users.noreply.github.com>
2024-05-09 18:42:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
-from function import heatCN
-import pytest
 import numpy as np
-
+import pytest
+import hypothesis
+from hypothesis import strategies as st
+from hypothesis import settings
+from hypothesis import given
+from function import heat_equation_CN, function_temperature
 
 test_cases = [
-    #peat thermal diffusivity
+    # peat thermal diffusivity
     {"length": 1.0, "nx": 2, "time": 1.0, "nt": 2, "alpha": 0.1},
     {"length": 21.0, "nx": 300, "time": 30.0, "nt": 1200, "alpha": 0.1},
-    #granite thermal diffusivity
+    # granite thermal diffusivity
     {"length": 47.0, "nx": 129, "time": 1.0, "nt": 50, "alpha": 1.3}
-    ]
+]
 
 
 @pytest.mark.parametrize("parameters", test_cases)
@@ -20,7 +23,8 @@ def test_boundary_conditions(parameters):
     nt = parameters["nt"]
     alpha = parameters["alpha"]
     
-    x_grid, w, b, A, B = heatCN(length, nx, time, nt, alpha)
+    x_grid = np.linspace(0, length, num=nx)
+    x_grid, w, b, A, B = heat_equation_CN(length, nx, time, nt, alpha, function_temperature(x_grid))
     
     assert np.all(w[0,:] == 0)
     assert np.all(w[-1,:] == 0)    
@@ -28,30 +32,23 @@ def test_boundary_conditions(parameters):
 
 @pytest.mark.parametrize("parameters", test_cases)
 def test_matrices_shape(parameters):
-    
     length = parameters["length"]
     nx = parameters["nx"]
     time = parameters["time"]
     nt = parameters["nt"]
     alpha = parameters["alpha"]
-   
+    #convert parameters to the correct types
     length = float(length)
     nx = int(nx)
     time = float(time)
     nt = int(nt)
     alpha = float(alpha)
+      
+    x_grid = np.linspace(0, length, num=nx)
+    x_grid, w, b, A, B = heat_equation_CN(length, nx, time, nt, alpha, function_temperature(x_grid))
         
-    x_grid, w, b, A, B = heatCN(length, nx, time, nt, alpha)
-        
+    #check dimensions of matrices A, B, C, w and, array b
     assert A.shape == (nx, nx)
     assert B.shape == (nx, nx)
     assert w.shape == (nx, nt)
     assert b.shape == (nx,)
-
-    
-
-
-
-
-if __name__ == "main":
-    pass        
