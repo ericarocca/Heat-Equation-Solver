79a3a8d7441530b0cdf6b88fe51eb5a1d2c0ba32
ericarocca <156796666+ericarocca@users.noreply.github.com>
2024-05-18 18:30:11 +0200

    Update function.py
    Analytical solutions

--- function.py
+++ function.py
@@ -7,8 +7,8 @@ import math
 #Crank-Nicolson function
 #Analytical function
 
-def function_temperature(x):
-    return np.ones_like(x) * 20
+def function_temperature(x, length):
+    return np.sin(np.pi * x)
 
 def heat_equation_CN(length, nx, time, nt, alpha, function_temperature):
     #nx = spatial steps
@@ -22,6 +22,7 @@ def heat_equation_CN(length, nx, time, nt, alpha, function_temperature):
     #function of temperature
 #    function_temperature = np.sin( np.pi * x_grid / length)
 #    function_temperature = np.cos(np.pi * x_grid)
+#    np.ones_like(x) * 20
     
     #create heat array 
     w = np.zeros([nx, nt])
@@ -81,7 +82,7 @@ def heat_equation_CN(length, nx, time, nt, alpha, function_temperature):
 
 #rivedi k
 
-def heat_equation_analytical(length, nx, time, nt,k):
+def heat_equation_analytical(length, nx, time, nt, k):
     
     #create heat array
     wa = np.zeros([nx, nt])
@@ -93,14 +94,29 @@ def heat_equation_analytical(length, nx, time, nt,k):
     #general solution
     #first create the fragments of Fourier series
     #then they combine to form the solution
-    for n in range (1,500):
+    for n in range (2,500):
         for i in range (nt-1):
-            c = (40 * (1-(-1)**n) )/(n*np.pi)
-            s = np.sin((n*np.pi*x[:])/length)
-            e = math.exp(-k*((n*np.pi)/length)**2*t[i])
+            e = math.exp(-np.pi**2 * k * n**2 *t[i])
+            s = np.sin(n * np.pi * x[:])
+            #c = (2 * n * ((-1)**n + 1) ) / (np.pi * (n + 1) * (n - 1))
+            #s = np.sin((n * np.pi * x[:]))
+            #e = math.exp(-0.5*((n*np.pi)**2)*t[i])
             
-            
-            wa[:,i] = wa[:,i] + c * s * e
+          
+            wa[:,i] = wa[:,i] + (s * e)
     
     
     return x, wa
+
+def analytical_solution(x, timesteps, alpha, L, N):
+    x = np.linspace(0, L, N)
+    solutions = np.zeros((len(timesteps), len(x)))
+    for i, t in enumerate(timesteps):
+        solution_t = np.zeros_like(x)
+        for n in range(1, 10):  # Adjust range for more terms in the series
+            solution_t += np.exp(-np.pi**2 * alpha * n**2 * t) * np.sin(n * np.pi * x / L)
+        solutions[i, :] = solution_t
+    return solutions
+
+
+#np.exp(-np.pi**2 * alpha * n**2 * t) * np.sin(n * np.pi * x / L)
