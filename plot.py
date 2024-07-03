c3b4b72a6a24caedcd3e02f407ffb43e302af191
ericarocca <156796666+ericarocca@users.noreply.github.com>
2024-07-03 17:38:07 +0200

    Update plot.py
    Plot numerical VS analytical solution and surface plot of numerical solution

--- plot.py
+++ plot.py
@@ -2,29 +2,49 @@ from function import heat_equation_CN, function_temperature
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Convergence study
-length = 1.0
-time = 1.0
-alpha = 2.73  #quartzite typical diffusivity coefficient
+def plot_solutions(x, w, wa, nt, time):
+    """
+    This function plots the comparison between the numerical and the analytical 
+    solution of the heat equation evolving through different time steps
 
-#different time step sizes
-nt_values = [ 55, 100, 250, 500]
+    Parameters
+    ----------
+    x : spatial steps of both the solutions.
+    w : array of the temperature calculated with the numerical method.
+    wa : array of the temperature calculated with the analytical method.
+    nt : time steps of both the solutions.
+    time : total time of the evolution.
+    """
+    plt.figure(figsize=(12, 6))
+    timesteps = [0, int(nt/3), int(2*nt/3), nt-1]
+    
+    for i in timesteps:
+        plt.plot(x, w[:, i], label=f'Numerical t={i*time/(nt-1):.2f}')
+        plt.plot(x, wa[:, i], '--', label=f'Analytical t={i*time/(nt-1):.2f}')
+    
+    plt.xlabel('Position')
+    plt.ylabel('Temperature')
+    plt.legend()
+    plt.title('Comparison of Numerical and Analytical Solutions of the Heat Equation')
+    plt.show()
 
-#different spatial step sizes
-nx_values = [ 55, 100, 250, 500]
-
-
-fig, axes = plt.subplots(len(nt_values), len(nx_values), figsize=(12, 12), sharex=True, sharey=True)
-
-for i, nt in enumerate(nt_values):
-    for j, nx in enumerate(nx_values):
-        x_grid = np.linspace(0, length, num=nx)
-        x_grid, w, b, A, B = heat_equation_CN(length, nx, time, nt, alpha, function_temperature(x_grid))
-        axes[i, j].plot(x_grid, w[:, -1], label=f'nx={nx}\nnt={nt}')
-        axes[i, j].set_title(f'nx={nx}, nt={nt}')
-        axes[i, j].legend()
-
-fig.text(0.5, 0.04, 'x', ha='center')
-fig.text(0.04, 0.5, 'Temperature', va='center', rotation='vertical')
-plt.tight_layout()
-plt.show()
+def plot_surface_solution(x, w, nt, time):
+    """
+    This function plots the temperature as a function of both position and time.
+    
+    Parameters
+    ----------
+    x : spatial steps of the numerical solution.
+    w : array of the temperature calculated with the numerical method.
+    nt : time steps of the numerical solution.
+    time : total time of the evolution.
+    """
+    X, T = np.meshgrid(x, np.linspace(0, time, nt))
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, T, w.T, cmap='viridis')
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Time')
+    ax.set_zlabel('Temperature')
+    ax.set_title('3D Surface Plot of the Heat Equation Solution')
+    plt.show()
