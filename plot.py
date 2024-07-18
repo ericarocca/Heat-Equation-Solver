829f7c8ab6a6c50371af6d21965fc57f4df45306
ericarocca <156796666+ericarocca@users.noreply.github.com>
2024-07-18 11:33:43 +0200

    Update plots' titles

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from function import function_temperature, heat_equation_CN, heat_equation_analytical
 
-def plot_solutions(x, w, wa, nt, time):
+def plot_solutions(x, w, wa, nt, time, length, nx, alpha):
     """
     This function plots the comparison between the numerical and the analytical 
     solution of the heat equation evolving through different time steps
@@ -13,6 +13,9 @@ def plot_solutions(x, w, wa, nt, time):
     wa : array of the temperature calculated with the analytical method.
     nt : time steps of both the solutions.
     time : total time of the evolution.
+    length : length of the rod.
+    nx : number of spatial steps.
+    alpha : thermal diffusivity constant.
     """
     plt.figure(figsize=(12, 6))
     timesteps = [0, int(nt/3), int(2*nt/3), nt-1]
@@ -24,10 +27,11 @@ def plot_solutions(x, w, wa, nt, time):
     plt.xlabel('Position')
     plt.ylabel('Temperature')
     plt.legend()
-    plt.title('Comparison of Numerical and Analytical Solutions of the Heat Equation')
+    plt.title(f'Comparison of Numerical and Analytical Solutions of the Heat Equation\n'
+              f'Length={length}, nx={nx}, Time={time}, nt={nt}, Alpha={alpha}')
     plt.show()
 
-def plot_surface_solution(x, w, nt, time):
+def plot_surface_solution(x, w, nt, time, length, nx, alpha):
     """
     This function plots the temperature as a function of both position and time.
     
@@ -37,6 +41,9 @@ def plot_surface_solution(x, w, nt, time):
     w : array of the temperature calculated with the numerical method.
     nt : time steps of the numerical solution.
     time : total time of the evolution.
+    length : length of the rod.
+    nx : number of spatial steps.
+    alpha : thermal diffusivity constant.
     """
     X, T = np.meshgrid(x, np.linspace(0, time, nt))
     fig = plt.figure(figsize=(12, 6))
@@ -45,5 +52,6 @@ def plot_surface_solution(x, w, nt, time):
     ax.set_xlabel('Position')
     ax.set_ylabel('Time')
     ax.set_zlabel('Temperature')
-    ax.set_title('3D Surface Plot of the Heat Equation Numerical Solution')
+    ax.set_title(f'3D Surface Plot of the Heat Equation Numerical Solution\n'
+                 f'Length={length}, nx={nx}, Time={time}, nt={nt}, Alpha={alpha}')
     plt.show()
